10688d234d4f92cfc12c138c8009d2a81b7bcaed
rmdhannni <ggdani137@gmail.com>
2022-10-28 21:45:49 -0700

    reactjs1

---
+++ reactjs-1/src/App.jsx
@@ -0,0 +1,47 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Parentbox">
+      <div className="Foto">
+        <img src="rx7.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p className="Cate">Cars</p>
+        <h1 className="Title">Mazda Rx 7</h1>
+        <p className="Price">IDR 1.999.000.999</p>
+        <p className="Spesifikasi">
+          Spesifikasi Mazda rx7 Mazda RX 7 (FD) 2.6 Rotary TT (276 HP)
+          SylinderR2. Kapasitas Mesin2616 cm3. Tenaga203 KW @ 6500 RPM 276 HP @
+          6500 RPM 272 BHP @ 6500 RPM. Torsi218 lb-ft @ 5000 RPM 296 Nm @ 5000
+          RPM. Bahan BakarGasoline. Kapasitas bahan bakar-Sistem Bahan
+          BakarElectronic Fuel Injection.
+        </p>
+      </div>
+    </div>
+  );
+}
+function App2() {
+  return (
+    <div className="Parentbox">
+      <div className="Foto">
+        <img src="rx7.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p className="Cate">Cars</p>
+        <h1 className="Title">Mazda Rx 7</h1>
+        <p className="Price">IDR 1.999.000.999</p>
+        <p className="Spesifikasi">
+          Spesifikasi Mazda rx7 Mazda RX 7 (FD) 2.6 Rotary TT (276 HP)
+          SylinderR2. Kapasitas Mesin2616 cm3. Tenaga203 KW @ 6500 RPM 276 HP @
+          6500 RPM 272 BHP @ 6500 RPM. Torsi218 lb-ft @ 5000 RPM 296 Nm @ 5000
+          RPM. Bahan BakarGasoline. Kapasitas bahan bakar-Sistem Bahan
+          BakarElectronic Fuel Injection.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
